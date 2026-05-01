f89a8ddec955655db1dfbf5ace5e159355c1968b
Tabre <tabre@tabre.com>
2026-05-01 17:01:53 -0500

    fix: remove commented line

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ async function connectDB() {
         console.log('MongoDB connected');
     } catch (err) {
         console.error('MongoDB connection error:', err);
-        // process.exit(1);
     }
 }
 connectDB();
